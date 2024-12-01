c86add2ac27ae12f3e75d2eb97ceefe8b30f8bdd
Alex McIntyre <alexjmcintyre@gmail.com>
2024-12-01 09:07:44 +0000

    Initial commit

---
+++ 1a.py
@@ -0,0 +1,20 @@
+f = open("1_real.txt")
+fl = f.readlines()
+
+leftList = []
+rightList = []
+for line in fl:  # load cards into list
+    line = line.strip()
+    line = line.split("   ")
+    leftList.append(int(line[0]))
+    rightList.append(int(line[1]))
+
+leftList.sort()
+rightList.sort()
+
+dist = []
+for pair in range(len(leftList)):
+    dist.append(abs(leftList[pair]-rightList[pair]))
+
+result = sum(dist)
+print(result)
